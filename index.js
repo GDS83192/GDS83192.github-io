fb1a03a0b93bd53a8d71a36dd5a1c090127b764b
GDS83192 <Haku#83192>
2017-04-23 09:11:52 -0400

    Few new things

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function myFunction() {
     document.getElementById("demo").style.color = "red";
 }
+
+var btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random()*number);
+}
+
+btn.onclick = function() {
+  var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
