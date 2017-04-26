81a7255d389123cc5a53b5ec74633f9596c94b7d
GDS83192 <Haku#83192>
2017-04-26 10:34:47 -0400

    changes

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-function myFunction() {
-    document.getElementById("demo").style.color = "red";
-}
-
-var btn = document.querySelector('button');
-
-function random(number) {
-  return Math.floor(Math.random()*number);
-}
-
-btn.onclick = function() {
-  var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
-}
-
-
-
-
-
-document.body.onload = addElement;
-
-function addElement () {
-  // create a new div element
-  // and give it some content
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent); //add the text node to the newly created div.
-
-  // add the newly created element and its content into the DOM
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+function chBackcolor(color) {
+   document.body.style.background = color;
 }
